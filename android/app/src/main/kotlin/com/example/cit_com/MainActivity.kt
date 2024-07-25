cd91d3a6cf42215d32600cc5241b8c1e3c8fb636
S <shakibmohammad18@gmail.com>
2024-07-25 23:47:22 +0600

    new project citcom app start

---
+++ android/app/src/main/kotlin/com/example/cit_com/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cit_com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utilitis/consts/text_class.dart
@@ -0,0 +1,50 @@
+class RTexts {
+  //Splash Screen Text
+  static const String splash = "Cit";
+  static const String splash1 = "com";
+//Splash Screen Text
+
+//Account Page Text
+  static const String account = "Already a Customer?Sign In";
+  static const String account1 = "New to Citcom? Create an Account";
+  static const String account2 = "Skip sign in";
+//Account Page Text
+
+//Sign In Text
+  static const String signIn = "Enter your mobile number";
+  static const String signIn1 = "Password";
+  static const String signIn2 = "Forget Password";
+  static const String signIn3 = "Sign In";
+  static const String signIn4 = "Don’t have an account?";
+  static const String signIn5 = " Sign Up";
+//Sign In Text
+
+//Sign Up Text
+  static const String signUp = "Create a New account";
+  static const String fastL = "Fast and last name";
+  static const String mB = "Mobile Number";
+  static const String cP = "Create a password";
+  static const String bY = "By proceeding, you agree to zdrop’s";
+  static const String tC = "Teams and Conditions";
+  static const String by2 = "By proceeding, you agree to zdrop’s";
+  static const String pP = "Privacy and Policy";
+  static const String sU = "Sign Up";
+  static const String aH = "Already have an account? Sign In";
+//Sign Up Text
+
+//Forget Password Text
+  static const String forgetP = "Forget Password?";
+  static const String otP =
+      "Don’t worry we will sand an OTP to your registered mobile number";
+  static const String mN = "Enter your mobile number";
+  static const String cT = "Continue";
+//Forget Password Text
+
+//Verification Code Text
+  static const String vC = "Verification Code";
+  static const String vC1 = "verification Code";
+  static const String sC = "Resend in 59s";
+  static const String sT = "Verification Code has been sent ";
+  static const String next = "Next";
+//Verification Code Text
+}
